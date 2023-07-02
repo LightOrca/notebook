03286f1260e0edead6d8c4cc7856e5e8184bd861
Light <138037298+LightOrca@users.noreply.github.com>
2023-07-02 02:32:20 +0100

    Create light.py

---
+++ light.py
@@ -0,0 +1,3 @@
+names = input("enter the names of the humans you want to dispose of: ")
+method = input("How would you like them to perish? ")
+time = input("What time do you want to this happen? ")
